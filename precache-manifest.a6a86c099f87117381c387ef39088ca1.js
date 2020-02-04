8a9672141e22f75cbb036d64795caa21868ec2c1
vaibhav <vaibhav.frnz4u@gmail.com>
2020-02-05 04:42:44 +0530

    Updates

---
+++ precache-manifest.a6a86c099f87117381c387ef39088ca1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58f50bb987b84c29cfdf14c4a245d58c",
+    "url": "/textToTable/index.html"
+  },
+  {
+    "revision": "647951758c68f7d49b3c",
+    "url": "/textToTable/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "6a2b2cdf4b9cb6ad2eac",
+    "url": "/textToTable/static/css/main.a4b810c1.chunk.css"
+  },
+  {
+    "revision": "647951758c68f7d49b3c",
+    "url": "/textToTable/static/js/2.5e6d0db4.chunk.js"
+  },
+  {
+    "revision": "6aeb469c0c20fd3334c86de961c4692c",
+    "url": "/textToTable/static/js/2.5e6d0db4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6a2b2cdf4b9cb6ad2eac",
+    "url": "/textToTable/static/js/main.c38b833f.chunk.js"
+  },
+  {
+    "revision": "492835be28b68dff9afe",
+    "url": "/textToTable/static/js/runtime-main.3851b68b.js"
+  }
+]);
